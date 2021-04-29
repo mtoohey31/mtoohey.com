2044aed2a848e177eab8c2b230b083b94b79a8c0
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-04-29 10:51:52 -0400

    fix: fixed broken tag links on article pages

--- src/lib/TagList.svelte
+++ src/lib/TagList.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   export let tags: Array<String>;
   export let caps = true;
-  export let updateTags: (tag: String) => void = () => {};
+  export let updateTags: (tag: String) => void;
 </script>
 
 <span>{caps ? "T" : "t"}ags: </span>
-{#each tags.slice(0, -1) as tag}
-  <a on:click={() => updateTags(tag)}>{tag}</a><span>,</span>
-{/each}<a on:click={() => updateTags(tags[tags.length - 1])}
-  >{tags[tags.length - 1]}</a
->
+{#if updateTags}{#each tags.slice(0, -1) as tag}
+    <a on:click={() => updateTags(tag)}>{tag}</a><span>,</span>
+  {/each}<a on:click={() => updateTags(tags[tags.length - 1])}
+    >{tags[tags.length - 1]}</a
+  >{:else}{#each tags.slice(0, -1) as tag}
+    <a href="/articles?tag={encodeURIComponent(tag)}">{tag}</a><span>,</span>
+  {/each}<a on:click={() => updateTags(tags[tags.length - 1])}
+    >{tags[tags.length - 1]}</a
+  >{/if}
