e84f7534d2001f81fc66126afadd0296e76f051a
Matthew Toohey <contact@mtoohey.com>
2025-04-26 18:44:38 -0400

    update flake and caddy-cloudflare

--- nix/pkgs/servers/caddy-cloudflare/default.nix
+++ nix/pkgs/servers/caddy-cloudflare/default.nix
@@ -4,6 +4,6 @@ buildGoModule {
   pname = "caddy-cloudflare";
   version = "v2.9.1";
   src = ./.;
-  vendorHash = "sha256-Z8jdMpa4BSR9zAblHM5SNDZkyw0RlEBzvK1thMtrZl8=";
+  vendorHash = "sha256-x/6nDUO8h0KLWpNfWdGd9MYcHV2ZZjONWds8+Jc9Nxc=";
   meta.mainProgram = "caddy";
 }
