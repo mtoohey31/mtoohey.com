97de7b1b42636375a848ffa49af234af64933e29
Matthew Toohey <contact@mtoohey.com>
2025-12-10 23:21:46 -0500

    update caddy-cloudflare

--- nix/pkgs/servers/caddy-cloudflare/default.nix
+++ nix/pkgs/servers/caddy-cloudflare/default.nix
@@ -4,6 +4,6 @@ buildGoModule {
   pname = "caddy-cloudflare";
   version = "v2.10.2";
   src = ./.;
-  vendorHash = "sha256-JlD6t078VbovFIB/TQ28dwDQHNT5ekIIR4Avv+OqXOU=";
+  vendorHash = "sha256-ix+W0+/SWHpRlA+qJftu17TEPI/yqF2N/GDYjGpiiNA=";
   meta.mainProgram = "caddy";
 }
